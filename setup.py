dd553e0fa4be1089bcc639e9a8804d44e61fa430
Thomas Maxwell <thomas.maxwell@nasa.gov>
2017-07-18 18:13:22 -0400

    Add pyedas

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'pyedas',
+  packages = ['pyedas','pyedas.kernels','pyedas.kernels.internal','pyedas.portal'],
+  version = '1.2.1',
+  package_dir = {'': 'python/src'},
+  description = 'Python portal and worker components of the Earth Data Analytic Services (EDAS) framework',
+  author = 'Thomas Maxwell',
+  author_email = 'thomas.maxwell@nasa.gov',
+  url = 'https://github.com/nasa-nccs-cds/EDAS.git',
+  download_url = 'https://github.com/nasa-nccs-cds/EDAS/tarball/1.2.1',
+  keywords = ['climate', 'data', 'analytic', 'services'],
+  license = 'GNU GENERAL PUBLIC',
+  classifiers = [],
+)
