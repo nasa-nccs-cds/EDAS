3749f92e4f989fa4761cf4628595b93ed449e768
Thomas Maxwell <thomas.maxwell@nasa.gov>
2017-07-19 15:15:39 -0400

    Change DASSPLotTest

--- python/test/localPerformanceTests/DASSPlotTest.py
+++ python/test/localPerformanceTests/DASSPlotTest.py
@@ -14,14 +14,14 @@ try:
     response_manager = portal.createResponseManager()
     datainputs = """[domain=[{"name":"d0","lat":{"start":70,"end":90,"system":"values"},"lon":{"start":5,"end":45,"system":"values"}}],variable=[{"uri":"file:///dass/nobackup/tpmaxwel/.edas/cache/collections/NCML/CIP_MERRA2_6hr_tas.ncml","name":"tas:v1","domain":"d0"}],operation=[{"name":"CDSpark.average","input":"v1","domain":"d0","axes":"xy"}]]"""
 
-    rId1 = portal.sendMessage("execute", [ "WPS", datainputs, '{ "response":"file" }'] )
-    fileResponses = response_manager.getResponseVariables(rId1)
+    rId1 = portal.sendMessage("execute", [ "WPS", datainputs, '{ "response":"object" }'] )
+    responses = response_manager.getResponseVariables(rId1)
 
-    print "PLotting " + str(len(fileResponses)) + " responses"
-    fileVar = fileResponses[0](squeeze=1)
+    print "PLotting " + str(len(responses)) + " responses"
+    resultVar = responses[0](squeeze=1)
 
-    timeAxis = fileVar.getTime()
-    data = fileVar.data
+    timeAxis = resultVar.getTime()
+    data = resultVar.data
     list_of_datetimes = [ datetime.datetime(x.year,x.month,x.day,x.hour,x.minute,int(x.second)) for x in timeAxis.asComponentTime() ]
     dates = matplotlib.dates.date2num(list_of_datetimes)
 
