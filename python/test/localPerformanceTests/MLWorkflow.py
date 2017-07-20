450008f12a31d641054eec689a2fd070a8e0ae01
Thomas Maxwell <thomas.maxwell@nasa.gov>
2017-07-20 17:40:53 -0400

    Add MLPipeline

---
+++ python/test/localPerformanceTests/MLWorkflow.py
@@ -0,0 +1,32 @@
+from pyedas.portal.edas import *
+import matplotlib.pyplot as plt
+import datetime, matplotlib
+
+# assumes the java/scala side of the EDASPortal has been started using the startupEDASPortal.py script.
+
+
+request_port = 5670
+response_port = 5671
+edas_server = "10.71.9.11"
+
+try:
+    portal = EDASPortal( ConnectionMode.CONNECT, edas_server, request_port, response_port )
+    response_manager = portal.createResponseManager()
+    #    datainputs = """[domain=[{"name":"d0","lat":{"start":70,"end":90,"system":"values"},"lon":{"start":5,"end":45,"system":"values"}}],variable=[{"uri":"file:///dass/nobackup/tpmaxwel/.edas/cache/collections/NCML/CIP_MERRA2_6hr_tas.ncml","name":"tas:v1","domain":"d0"}],operation=[{"name":"CDSpark.average","input":"v1","domain":"d0","axes":"xy"}]]"""
+    datainputs = """[domain=[{"name":"d0","lat":{"start":0,"end":80,"system":"values"}}],variable=[{"uri":"file:///dass/nobackup/tpmaxwel/.edas/cache/collections/NCML/CIP_MERRA2_mon_tas.ncml","name":"tas:v1","domain":"d0"}],operation=[{"name":"CDSpark.binAve","input":"v1","domain":"d0","bin":"month","axes":"xt"}]]"""
+
+    rId1 = portal.sendMessage("execute", [ "WPS", datainputs, '{ "response":"object" }'] )
+    responses = response_manager.getResponseVariables(rId1)
+
+    print "Received " + str(len(responses)) + " responses"
+    resultVar = responses[0]
+
+    print "Result shape: " + str(resultVar.shape)
+
+    print "Sanple data: " + str(resultVar.data.flatten()[0:30])
+
+
+finally:
+    portal.shutdown()
+
+
