19241dd8ed4ef4b6f2f7facc595a4d08ef1dafd8
Thomas Maxwell <thomas.maxwell@nasa.gov>
2017-07-20 16:55:27 -0400

    Diagnostics

--- python/test/localPerformanceTests/DASSDiurnalCycle.py
+++ python/test/localPerformanceTests/DASSDiurnalCycle.py
@@ -1,5 +1,5 @@
 from pyedas.portal.edas import *
-import numpy as np
+import time, numpy as np
 
 request_port = 5670
 response_port = 5671
@@ -16,8 +16,10 @@ try:
     portal = EDASPortal( ConnectionMode.CONNECT, edas_server, request_port, response_port )
     response_manager = portal.createResponseManager()
     datainputs = """[domain=[{"name":"d0","lat":{"start":70,"end":90,"system":"values"},"lon":{"start":25,"end":45,"system":"values"}}],variable=[{"uri":"file:///dass/nobackup/tpmaxwel/.edas/cache/collections/NCML/MERRA_TAS1hr.ncml","name":"tas:v1","domain":"d0"}],operation=[{"name":"CDSpark.binAve","input":"v1","domain":"d0","cycle":"diurnal","bin":"month","axes":"t"}]]"""
+    t0 = time.time()
     rId1 = portal.sendMessage("execute", [ "WPS", datainputs, '{ "response":"object" }'] )
     responses = response_manager.getResponseVariables(rId1)
+    print "Completed OP in time {0}".format( time.time()-t0 );
 
     cycle_data = getCycle( responses, 5 )
     print str( cycle_data )
