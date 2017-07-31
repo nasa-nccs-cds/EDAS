435e68cd7c2f6ab8a6c6f7db26b0f31243debc1c
Thomas Maxwell <thomas.maxwell@nasa.gov>
2017-07-31 12:41:44 -0400

    Update get capabilities test

--- python/test/getCapabilitiesTest.py
+++ python/test/getCapabilitiesTest.py
@@ -3,16 +3,28 @@ import time, numpy as np
 
 request_port = 5670
 response_port = 5671
-# edas_server = "10.71.9.11"
-edas_server = "localhost"
+edas_server = "10.71.9.11"
+# edas_server = "localhost"
+test_collection = "cip_cfsr_6hr_ta"
 
 try:
     portal = EDASPortal( ConnectionMode.CONNECT, edas_server, request_port, response_port )
     response_manager = portal.createResponseManager()
     rId = portal.sendMessage("getCapabilities", [""])
     responses = response_manager.getResponses(rId)
-    print "Got responses:\n" + "\n".join(responses)
+    print "Capabilities:\n" + "\n".join(responses)
 
+    rId = portal.sendMessage("describeProcess", ["CDSpark.diff2"])
+    responses = response_manager.getResponses(rId)
+    print "Process:\n" + "\n".join(responses)
+
+    rId = portal.sendMessage("getCapabilities", ["coll"])
+    responses = response_manager.getResponses(rId)
+    print "\n\nList Collections:\n" + "\n".join(responses)
+
+    rId = portal.sendMessage("getCapabilities", [ "coll:" + test_collection ])
+    responses = response_manager.getResponses(rId)
+    print "\n\nCollection " + test_collection + " Metadata:\n" + "\n".join(responses)
 
 finally:
     portal.shutdown()
