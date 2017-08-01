2ab0595d764cb72fa18299f82dbb6197ac9c0493
Thomas Maxwell <thomas.maxwell@nasa.gov>
2017-08-01 15:08:59 -0400

    Diagnostics

--- python/test/fileResponseTest.py
+++ python/test/fileResponseTest.py
@@ -3,20 +3,18 @@ import time, sys, cdms2, os
 from pyedas.portal.edas import *
 from cdms2.variable import DatasetVariable
 
-startServer = False
-portal = None
 request_port = 5670
 response_port = 5671
-server = "localhost"
+edas_server = "10.71.9.11"
 
 try:
 
-    portal = EDASPortal(ConnectionMode.CONNECT, server, request_port, response_port)
+    portal = EDASPortal(ConnectionMode.CONNECT, edas_server, request_port, response_port)
     response_manager = portal.createResponseManager()
 
     t0 = time.time()
     datainputs = '[domain=[{"name":"d0","lat":{"start":0,"end":90,"system":"values"},"lon":{"start":30,"end":120,"system":"values"}}],variable=[{"uri":"collection:/cip_merra2_mon_tas","name":"tas:v1","domain":"d0"}]]'
-    print "Sending request on port {0}, server {1}: {2}".format( portal.request_port, server, datainputs ); sys.stdout.flush()
+    print "Sending request on port {0}, server {1}: {2}".format( portal.request_port, edas_server, datainputs ); sys.stdout.flush()
 
     rId = portal.sendMessage( "execute", [ "CDSpark.workflow", datainputs, '{ "response":"file" }'] )
     responses = response_manager.getResponseVariables(rId);   """:type : list[DatasetVariable] """
