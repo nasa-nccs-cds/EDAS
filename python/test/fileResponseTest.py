749f94caddda7f5dd29c4d288dd528efc5638660
Thomas Maxwell <thomas.maxwell@nasa.gov>
2017-08-01 13:03:56 -0400

    Update file test

--- python/test/fileResponseTest.py
+++ python/test/fileResponseTest.py
@@ -17,7 +17,7 @@ try:
     response_manager = portal.createResponseManager()
 
     t0 = time.time()
-    datainputs = '[domain=[{"name":"d0","lat":{"start":5,"end":40,"system":"values"},"lon":{"start":80,"end":120,"system":"values"}}],variable=[{"uri":"http://esgf.nccs.nasa.gov/thredds/dodsC/CMIP5/NASA/GISS/historical/E2-H_historical_r1i1p1/tas_Amon_GISS-E2-H_historical_r1i1p1_185001-190012.nc","name":"tas:v1","domain":"d0"}],operation=[{"name":"CDSpark.average","input":"v1","domain":"d0","axes":"xy"}]]'
+    datainputs = '[domain=[{"name":"d0","lat":{"start":0,"end":90,"system":"values"},"lon":{"start":30,"end":120,"system":"values"}}],variable=[{"uri":"collection:/cip_merra2_mon_tas","name":"tas:v1","domain":"d0"}]]'
     print "Sending request on port {0}, server {1}: {2}".format( portal.request_port, server, datainputs ); sys.stdout.flush()
 
     rId = portal.sendMessage( "execute", [ "CDSpark.workflow", datainputs, '{ "response":"file" }'] )
--- src/test/scala/TestSuite.scala
+++ src/test/scala/TestSuite.scala
@@ -243,7 +243,7 @@ class CurrentTestSuite extends FunSuite with Loggable with BeforeAndAfter {
     val result_data = CDFloatArray( getResultData( result_node ) ).sample( 35 )
     println( " ** CDMS Result:       " + result_data.mkDataString( ", " ) )
   }
-  
+
   test("IOTestNoOp") {
     val datainputs = s"""[domain=[{"name":"d0","time":{"start":0,"end":1,"system":"indices"},"level":{"start":0,"end":0,"system":"indices"}}],variable=[{"uri":"http://dataserver.nccs.nasa.gov/thredds/dodsC/bypass/CREATE-IP/reanalysis/CFSR/6hr/atmos/ta_2000s.ncml","name":"ta:v1","domain":"d0"}],operation=[{"name":"CDSpark.subset","input":"v1","domain":"d0"}]]"""
     val result_node = executeTest(datainputs)
