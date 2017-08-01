c601751a4e7ac8f35db024e6898218cc7e88ca2e
Thomas Maxwell <thomas.maxwell@nasa.gov>
2017-08-01 16:50:41 -0400

    add DASS ave test

---
+++ python/test/localPerformanceTests/DASSAvelTest.py
@@ -0,0 +1,44 @@
+from pyedas.portal.edas import *
+import time
+
+request_port = 5670
+response_port = 5671
+
+try:
+    portal = EDASPortal( ConnectionMode.CONNECT, "10.71.9.11", request_port, response_port )
+    response_manager = portal.createResponseManager()
+
+    datainputs = """[
+        domain=[{
+            "name":"d0",
+            "lat":{
+                "start":40.25,
+                "end":40.25,
+                "system":"values"
+            },
+            "lon":{
+                "start":89.75,
+                "end":89.75,
+                "system":"values"
+            }
+        }],
+        variable=[{
+            "uri":"file:///dass/nobackup/tpmaxwel/.edas/cache/collections/NCML/CIP_MERRA2_6hr_hur.ncml",
+            "name":"hur",
+            "domain":"d0"
+        }],
+        operation=[{
+            "name":"CDSpark.average",
+            "input":"hur",
+            "domain":"d0",
+            "axes":"xy"
+        }] ]"""
+    
+    rId = portal.sendMessage("execute", [ "WPS", datainputs, ""] )
+    responses = response_manager.getResponses(rId)
+    print "Got responses:\n" + "\n".join(responses)
+
+finally:
+    portal.shutdown()
+
+
