81517fea8a27dcb3a497b436f2b421048e750a1e
Thomas Maxwell <thomas.maxwell@nasa.gov>
2017-08-02 15:08:55 -0400

    Fix file mode errors; improve startup script

--- bin/startup_cdas_yarn.sh
+++ bin/startup_cdas_yarn.sh
@@ -4,5 +4,6 @@ SPARK_PRINT_LAUNCH_COMMAND=true
 APP_DEP_JARS=$(find ~/.ivy2 -name "*.jar" )
 APP_DEP_CP=$(echo $APP_DEP_JARS | sed -r 's/[ \n\r]+/:/g')
 echo "Application classpath:  $APP_DEP_CP "
+cd ${EDAS_HOME_DIR}; python ./python/src/pyedas/initialServiceRequest.py &
 $CDH_HOME/bin/spark-submit --class nasa.nccs.edas.portal.EDASApplication --master yarn --deploy-mode client --conf "spark.executor.extraClassPath=${APP_DEP_CP}" --conf "spark.driver.extraClassPath=${APP_DEP_CP}" --driver-memory 8g ${EDAS_JAR} bind 5670 5671 /home/tpmaxwel/.edas/cache/edas.properties.yarn
 
