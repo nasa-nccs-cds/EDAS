fe1e4c40cd473eb7b92aaef88fecbee9d1af07cb
Thomas Maxwell <thomas.maxwell@nasa.gov>
2017-08-07 14:58:04 -0400

    Diagnostics

--- python/test/localPerformanceTests/DASSNumpyModuleTest.py
+++ python/test/localPerformanceTests/DASSNumpyModuleTest.py
@@ -12,7 +12,7 @@ edas_server = "10.71.9.11"
 try:
     portal = EDASPortal( ConnectionMode.CONNECT, edas_server, request_port, response_port )
     response_manager = portal.createResponseManager()
-    datainputs = """[domain=[{"name":"d0","lat":{"start":8,"end":13,"system":"indices"},"lon":{"start":70,"end":72,"system":"indices"},"time":{"start":5,"end":10,"system":"indices"}}],variable=[{"uri":"file:///dass/nobackup/tpmaxwel/.edas/cache/collections/NCML/CIP_MERRA_mon_pr.ncml","name":"pr:v1","domain":"d0"}],operation=[{"name":"python.numpyModule.avew","input":"v1","domain":"d0","axes":"xy"}]]"""
+    datainputs = """[domain=[{"name":"d0","lat":{"start":8,"end":13,"system":"indices"},"lon":{"start":70,"end":72,"system":"indices"},"time":{"start":5,"end":10,"system":"indices"}}],variable=[{"uri":"file:///dass/nobackup/tpmaxwel/.edas/cache/collections/NCML/CIP_MERRA_mon_pr.ncml","name":"pr","domain":"d0"}],operation=[{"name":"python.numpyModule.avew","input":"pr","domain":"d0","axes":"xy"}]]"""
 
     rId1 = portal.sendMessage("execute", [ "WPS", datainputs ] )
     responses = response_manager.getResponseVariables(rId1)
