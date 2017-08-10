a6bf6bfee99c097b99281f9141a997d5daa3f92e
Thomas Maxwell <thomas.maxwell@nasa.gov>
2017-08-10 15:51:04 -0400

    Add EnsembleReadTest

--- python/test/localPerformanceTests/EnsembleReadTest.py
+++ python/test/localPerformanceTests/EnsembleReadTest.py
@@ -6,11 +6,11 @@ import numpy.ma as ma
 
 dset_address = "/dass/pubrepo/CREATE-IP/data/reanalysis/NASA-GMAO/GEOS-5/MERRA2/mon/atmos/tas/tas_Amon_reanalysis_MERRA2_198001-201412.nc"
 vname = "tas"
-plev = "100000, 97500, 95000, 92500, 90000, 87500, 85000, 82500, 80000, 77500, 75000, 70000, 65000, 60000, 55000, 50000, 45000, 40000, 35000, 30000, 25000, 20000, 15000, 10000"
+levs = (100000, 97500, 95000, 92500, 90000, 87500, 85000, 82500, 80000, 77500, 75000, 70000, 65000, 60000, 55000, 50000, 45000, 40000, 35000, 30000, 25000, 20000, 15000, 10000)
 
 t0 = time.time()
 dset = cdms2.open( dset_address )
-var = dset( vname, genutil.picker( plev=plev ) );  """:type : AbstractVariable """
+var = dset( vname, genutil.picker( plev=levs ) );  """:type : AbstractVariable """
 t1 = time.time()
 
 print "Completed read test in time {1}, shape = {2}".format( str(t1-t0), str(var.shape) )
