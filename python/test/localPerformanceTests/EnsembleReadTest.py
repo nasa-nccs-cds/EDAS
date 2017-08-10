13c7ecd316e0ea827f49dc7719861da3b768713f
Thomas Maxwell <thomas.maxwell@nasa.gov>
2017-08-10 14:57:06 -0400

    Add EnsembleReadTest

--- python/test/localPerformanceTests/EnsembleReadTest.py
+++ python/test/localPerformanceTests/EnsembleReadTest.py
@@ -4,7 +4,7 @@ import cdms2, time, os, cdutil, genutil
 import numpy as np
 import numpy.ma as ma
 
-dset_address = "file:///dass/nobackup/tpmaxwel/.edas/cache/collections/NCML/CIP_MERRA2_mon_tas.ncml"
+dset_address = "/dass/pubrepo/CREATE-IP/data/reanalysis/NASA-GMAO/GEOS-5/MERRA2/mon/atmos/tas/tas_Amon_reanalysis_MERRA2_198001-201412.nc"
 vname = "tas"
 plev = "100000, 97500, 95000, 92500, 90000, 87500, 85000, 82500, 80000, 77500, 75000, 70000, 65000, 60000, 55000, 50000, 45000, 40000, 35000, 30000, 25000, 20000, 15000, 10000"
 
