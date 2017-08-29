52a5b2ed77d28f987c5ebc0d3772d85bc0ed0671
Thomas Maxwell <thomas.maxwell@nasa.gov>
2017-08-29 17:25:55 -0400

    Update diagnostics

--- python/src/pyedas/initialServiceRequest.py
+++ python/src/pyedas/initialServiceRequest.py
@@ -10,19 +10,19 @@ try:
     portal = EDASPortal( ConnectionMode.CONNECT, edas_server, request_port, response_port )
     response_manager = portal.createResponseManager()
     rId = portal.sendMessage("getCapabilities", [""])
-    log_file.write( "getCapabilities request sent\n" )
+    log_file.write( "getCapabilities request sent\n" ); log_file.flush()
     responses = response_manager.getResponses(rId)
-    log_file.write( "Capabilities:\n" + "\n".join(responses) )
+    log_file.write( "Capabilities:\n" + "\n".join(responses) ); log_file.flush()
 
     rId = portal.sendMessage("getCapabilities", ["coll"])
-    log_file.write( "getCapabilities [ coll ] request sent\n" )
+    log_file.write( "getCapabilities [ coll ] request sent\n" ); log_file.flush()
     responses = response_manager.getResponses(rId)
-    log_file.write( "\n\nList Collections:\n" + "\n".join(responses) )
+    log_file.write( "\n\nList Collections:\n" + "\n".join(responses) ); log_file.flush()
 
 finally:
-    log_file.write( "shutdown" )
+    log_file.write( "shutdown" ); log_file.flush()
     portal.shutdown()
-    log_file.write( "close" )
+    log_file.write( "close" ); log_file.flush()
     log_file.close()
 
 
