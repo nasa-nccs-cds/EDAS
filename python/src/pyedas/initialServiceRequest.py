5bfc096631bda9cadfce77a87d8cf7e88618b585
Thomas Maxwell <thomas.maxwell@nasa.gov>
2017-08-29 17:19:41 -0400

    Update diagnostics

--- python/src/pyedas/initialServiceRequest.py
+++ python/src/pyedas/initialServiceRequest.py
@@ -3,22 +3,27 @@ import time, numpy as np
 
 request_port = 5670
 response_port = 5671
-# edas_server = "10.71.9.11"
-edas_server = "localhost"
+edas_server = "127.0.0.1"
 
 try:
+    log_file  = open( '/tmp/EDAS_init.log', 'w' )
     portal = EDASPortal( ConnectionMode.CONNECT, edas_server, request_port, response_port )
     response_manager = portal.createResponseManager()
     rId = portal.sendMessage("getCapabilities", [""])
+    log_file.write( "getCapabilities request sent\n" )
     responses = response_manager.getResponses(rId)
-    print "Capabilities:\n" + "\n".join(responses)
+    log_file.write( "Capabilities:\n" + "\n".join(responses) )
 
     rId = portal.sendMessage("getCapabilities", ["coll"])
+    log_file.write( "getCapabilities [ coll ] request sent\n" )
     responses = response_manager.getResponses(rId)
-    print "\n\nList Collections:\n" + "\n".join(responses)
+    log_file.write( "\n\nList Collections:\n" + "\n".join(responses) )
 
 finally:
+    log_file.write( "shutdown" )
     portal.shutdown()
+    log_file.write( "close" )
+    log_file.close()
 
 
 
