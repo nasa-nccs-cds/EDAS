eb13de59d40efc4569bc2f6b95e0014ffa96affe
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-11-12 19:08:07 -0500

    Diag

--- bin/startup_edas_standalone.sh
+++ bin/startup_edas_standalone.sh
@@ -4,6 +4,7 @@ SPARK_PRINT_LAUNCH_COMMAND=true
 APP_DEP_JARS=$(find ~/.ivy2 -name "*.jar" )
 APP_DEP_CP=$(echo $APP_DEP_JARS | sed -r 's/[ \n\r]+/:/g')
 echo "Application classpath:  $APP_DEP_CP "
+echo "EDAS jar:  $EDAS_JAR "
 rm ~/.edas/logs/edas-* ~/.edas/logs/python-*
 rm -rf $EDAS_CACHE_DIR/block* $EDAS_CACHE_DIR/spark*
 $SPARK_HOME/bin/spark-submit --class nasa.nccs.edas.portal.EDASApplication --master spark://cldralogin101:7077 --deploy-mode client --conf "spark.executor.extraClassPath=${APP_DEP_CP}" --conf "spark.driver.extraClassPath=${APP_DEP_CP}" --driver-memory 10G ${EDAS_JAR} 5670 5671 ${EDAS_CACHE_DIR}/edas.properties
