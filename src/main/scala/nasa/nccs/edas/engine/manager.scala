a8b101eba3149f5c195d99df9530ebe798c84b7b
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-11-19 17:19:21 -0500

    Add spark worker cleanup

--- src/main/scala/nasa/nccs/edas/engine/manager.scala
+++ src/main/scala/nasa/nccs/edas/engine/manager.scala
@@ -61,7 +61,7 @@ object CDS2ExecutionManager extends Loggable {
     val shutdown_script = Paths.get( sys.env("HOME"), ".edas", "sbin", "shutdown_python_worker.sh" ).toFile
     if( slaves_file.exists && slaves_file.canRead ) {
       val shutdown_futures = for (slave <- Source.fromFile(slaves_file).getLines(); slave_node = slave.trim; if !slave_node.isEmpty && !slave_node.startsWith("#") ) yield  {
-          Future {  try { "ssh %s \"%s\"".format(slave_node,shutdown_script.toString) ! }
+          Future {  try { "ssh %s %s".format(slave_node,shutdown_script.toString) ! }
                     catch { case err: Exception => println( "Error shutting down python workers on slave_node '" + slave_node + "' using shutdown script '" + shutdown_script.toString + "': " + err.toString ); }
           }
       }
@@ -82,7 +82,7 @@ object CDS2ExecutionManager extends Loggable {
     print( s"Cleaning up spark workers using slaves file ${slaves_file}.")
     if( slaves_file.exists && slaves_file.canRead ) {
       val shutdown_futures = for (slave <- Source.fromFile(slaves_file).getLines(); slave_node = slave.trim; if !slave_node.isEmpty && !slave_node.startsWith("#") ) yield  {
-        val remote_shutdown_script = "ssh %s \"%s\"".format(slave_node,shutdown_script.toString)
+        val remote_shutdown_script = "ssh %s %s".format(slave_node,shutdown_script.toString)
         print( s"\nCleaning up spark worker ${slave_node} with shutdown script: '${remote_shutdown_script}'"  )
         Future {  try { remote_shutdown_script ! }
         catch { case err: Exception => println( "Error shutting down spark workers on slave_node '" + slave_node + "' using shutdown script '" + shutdown_script.toString + "': " + err.toString ); }
