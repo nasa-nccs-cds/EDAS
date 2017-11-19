ee0130dd26ee1ea55a194cfccf00798f89bdfb04
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-11-19 17:25:14 -0500

    Add spark worker cleanup

--- src/main/scala/nasa/nccs/edas/engine/manager.scala
+++ src/main/scala/nasa/nccs/edas/engine/manager.scala
@@ -81,14 +81,12 @@ object CDS2ExecutionManager extends Loggable {
     val shutdown_script = Paths.get( sys.env("HOME"), ".edas", "sbin", "cleanup_spark_workers.sh" ).toFile
     print( s"Cleaning up spark workers using slaves file ${slaves_file}.")
     if( slaves_file.exists && slaves_file.canRead ) {
-      val shutdown_futures = for (slave <- Source.fromFile(slaves_file).getLines(); slave_node = slave.trim; if !slave_node.isEmpty && !slave_node.startsWith("#") ) yield  {
+      for (slave <- Source.fromFile(slaves_file).getLines(); slave_node = slave.trim; if !slave_node.isEmpty && !slave_node.startsWith("#") ) {
         val remote_shutdown_script = "ssh %s %s".format(slave_node,shutdown_script.toString)
         print( s"\nCleaning up spark worker ${slave_node} with shutdown script: '${remote_shutdown_script}'"  )
-        Future {  try { remote_shutdown_script ! }
+        try { remote_shutdown_script ! }
         catch { case err: Exception => println( "Error shutting down spark workers on slave_node '" + slave_node + "' using shutdown script '" + shutdown_script.toString + "': " + err.toString ); }
-        }
       }
-      Future.sequence( shutdown_futures )
     } else try {
       logger.info( "No slaves file found, shutting down python workers locally:")
       try { shutdown_script.toString ! }
