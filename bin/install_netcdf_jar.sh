e65429a8b13078976e9745cb34e51afb1a694b20
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-12-09 13:08:55 -0800

    Add nc install script

---
+++ bin/install_netcdf_jar.sh
@@ -0,0 +1,4 @@
+cd $EDAS_HOME_DIR
+mkdir lib
+cd lib
+wget --no-check-certificate http://artifacts.unidata.ucar.edu/content/repositories/unidata-releases/edu/ucar/netcdfAll/4.6.11/netcdfAll-4.6.11.jar   
