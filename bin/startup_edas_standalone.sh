c7300c597d0aefef99f4445e65a4a638b8bf4fae
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2017-12-13 15:44:06 -0600

    Config changes

--- bin/startup_edas_standalone.sh
+++ bin/startup_edas_standalone.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 SCALA_VERSION=2.11
 EDAS_JAR=${EDAS_HOME_DIR}/target/scala-${SCALA_VERSION}/edas_${SCALA_VERSION}-${EDAS_VERSION}-SNAPSHOT.jar
-SPARK_PRINT_LAUNCH_COMMAND=true 
-APP_DEP_JARS=$(find ~/.ivy2 -name "*.jar" )
-APP_DEP_CP=$(echo $APP_DEP_JARS | sed -r 's/[ \n\r]+/:/g')
+SPARK_PRINT_LAUNCH_COMMAND=true
+APP_DEP_JARS=$(find ${EDAS_HOME_DIR}/lib -name "*.jar" )
+IVY_JARS=$(find ~/.ivy2 -name "*.jar" )
+APP_DEP_CP=$(echo $APP_DEP_JARS | sed -r 's/[ \n\r]+/:/g'):$(echo $IVY_JARS | sed -r 's/[ \n\r]+/:/g')
 echo "Application classpath:  $APP_DEP_CP "
 echo "EDAS jar:  $EDAS_JAR "
 rm /tmp/${USER}/logs/*
