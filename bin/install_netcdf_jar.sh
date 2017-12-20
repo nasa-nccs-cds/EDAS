c11c756d3f253f0819ca465791a44ef6240916e5
Thomas Maxwell <thomas.maxwell@nasa.gov>
2017-12-20 14:51:46 -0500

    Remove netcdf install script

--- bin/install_netcdf_jar.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cd $EDAS_HOME_DIR
-mkdir lib
-cd lib
-wget --no-check-certificate http://artifacts.unidata.ucar.edu/content/repositories/unidata-releases/edu/ucar/netcdfAll/4.6.11/netcdfAll-4.6.11.jar
-
