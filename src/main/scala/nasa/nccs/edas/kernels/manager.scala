e1d622d1df5b5f63be5dca98fd7ebbd1abc23ab6
Thomas Maxwell <thomas.maxwell@nasa.gov>
2018-02-12 17:45:18 -0500

    Diag

--- src/main/scala/nasa/nccs/edas/kernels/manager.scala
+++ src/main/scala/nasa/nccs/edas/kernels/manager.scala
@@ -1,9 +1,11 @@
 package nasa.nccs.edas.kernels
 import nasa.nccs.edas.utilities.appParameters
 import nasa.nccs.edas.workers.python.PythonWorkerPortal
+
 import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
-import nasa.nccs.utilities.cdsutils
+import nasa.nccs.utilities.{Loggable, cdsutils}
+
 import scala.collection.immutable.Map
 
 class KernelMgr(  ) {
@@ -31,7 +33,7 @@ class KernelMgr(  ) {
 
 }
 
-object KernelPackageTools {
+object KernelPackageTools extends Loggable {
   import com.google.common.reflect.ClassPath
   val internalKernelsPackage = "nasa.nccs.edas.modules"
   val externalKernelPackages = cdsutils.envList("EDAS_KERNEL_PACKAGES")
@@ -46,6 +48,7 @@ object KernelPackageTools {
     val visLevel = KernelStatus.parse( if( visibility.isEmpty ) { appParameters("kernels.visibility","public") } else visibility )
     val internal_kernels: Map[String,KernelModule] = getKernelClasses.map(ClassInfoRec( _ )).groupBy( _.module.toLowerCase ).mapValues( KernelModule(_) filter visLevel )
     val capabilities_data = PythonWorkerPortal.getInstance().getCapabilities()
+    logger.info( " #PK# Loading python kernels from capabilities data: " + capabilities_data.mkString(", "))
     val python_kernels: Array[KernelModule] = capabilities_data map ( KernelModule(_) filter visLevel )
     val external_kernel_map: Map[String,KernelModule] = Map( python_kernels.map( km => km.getName -> km ): _* )
     ( internal_kernels ++ external_kernel_map ) filter { case (_,kmod) => kmod.nonEmpty }
