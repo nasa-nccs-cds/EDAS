e1ae92b7a4c45e6784dfa69bbbd073312585861a
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2018-02-19 12:10:27 -0500

    DIAG

--- src/main/scala/nasa/nccs/edas/rdd/container.scala
+++ src/main/scala/nasa/nccs/edas/rdd/container.scala
@@ -468,7 +468,6 @@ class RDDContainer extends Loggable {
   def section( section: CDSection ): Unit = vault.map( _.section(section) )
   def release( keys: Iterable[String] ): Unit = { vault.release( keys ) }
   def variableRecs: Map[String,VariableRecord] = value.variableRecords
-  def nSlices = _vault.fold(-1)( _.nSlices.toInt )
 
   private def initialize( init_value: TimeSliceRDD, contents: List[String] ) = {
     _vault = Some( new RDDVault( init_value ) )
