008ab286f6d0f409fe0bc5cdc4d0e0f5f88d144a
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2018-03-17 13:46:43 -0400

    Configuration

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Versions {
   val ucar = "4.6.8"
-  val spark = "2.2.1"
+  val spark = "2.3.0"
   val jackson = "2.6.5"
   val breeze = "0.13.2"
 }
@@ -33,17 +33,12 @@ object Library {
   val natty          = "com.joestelmach"    % "natty"           % "0.12"
   val py4j           = "net.sf.py4j"        % "py4j"            % "0.10.4"
   val geotools       = "org.geotools"      %  "gt-shapefile"    % "13.2"
-
-  val sprayCache     = "io.spray"       % "spray-caching_2.11" % "1.3.3"
-  val sprayUtil      = "io.spray"       % "spray-util_2.11"    % "1.3.3"
   val scalactic      = "org.scalactic" %% "scalactic"          % "3.0.0"
   val scalatest      = "org.scalatest" %% "scalatest" % "3.0.0" % "test"
   val concurrentlinkedhashmap = "com.googlecode.concurrentlinkedhashmap" % "concurrentlinkedhashmap-lru" % "1.4.2"
   val reflections    = "org.reflections" % "reflections"       % "0.9.10"
-
   val scalaxml       = "org.scala-lang.modules" %% "scala-xml"  % "1.0.3"
   val scalaparser    = "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.3"
-
   val jacksonCore = "com.fasterxml.jackson.core" % "jackson-core" % Versions.jackson
   val jacksonDatabind = "com.fasterxml.jackson.core" % "jackson-databind" % Versions.jackson
   val jacksonModule = "com.fasterxml.jackson.module" % "jackson-module-scala_2.10" % Versions.jackson
