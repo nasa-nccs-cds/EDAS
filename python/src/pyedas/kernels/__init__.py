defaba76aa9922437fcfc066ce128ec58b9cd03d
nasa-nccs-cds <tpmaxwel@nccs.nasa.gov>
2018-05-11 17:20:38 -0400

    udpate logging

--- python/src/pyedas/kernels/__init__.py
+++ python/src/pyedas/kernels/__init__.py
@@ -3,7 +3,7 @@ import socket
 
 lname = "worker"
 lFilename = "edas-python-" + lname + "-" + socket.gethostname() + "-" + str(os.getpid())
-log_file = os.path.expanduser('/tmp/' + getpass.getuser() + '/logs/' + lFilename  +'.log')
+log_file = os.path.expanduser( '~/.edas/logs/' + lFilename  +'.log')
 logger = logging.getLogger()
 formatter = logging.Formatter(lname + ': %(asctime)s %(levelname)s %(message)s')
 handler = logging.FileHandler( log_file )
